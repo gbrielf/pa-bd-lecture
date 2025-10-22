10dc7f32a75659faea5cc2efb110567c79a1864b
Gabriel Henrique F. Barbosa <56904537+gbrielf@users.noreply.github.com>
2025-10-22 12:52:41 +0000

    finalizando serializers e models, irei iniciar views e urls e configuracao do swagger em seguida

--- activy-15-10/kanban/core/serializers.py
+++ activy-15-10/kanban/core/serializers.py
@@ -1,11 +1,47 @@
 from rest_framework import serializers
-from models import Projeto, Coluna, Tarefa, Comentario, Etiqueta, Usuario
+from .models import Projeto, Coluna, Tarefa, Comentario, Etiqueta, Usuario
+
+
+class EtiquetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Etiqueta
+        fields = ['id','nome', 'cor']
+        read_only_fields = ['id']
+
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = ['id', 'username', 'email']
         read_only_fields = ['id']
+
+
+class ColunaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coluna
+        fields = ['id','titulo', 'ordem', 'projeto']
+        read_only_fields = ['id']
+
+
+class TarefaSerializer(serializers.ModelSerializer):
+    coluna = ColunaSerializer(many=True, read_only=True)
+    responsavel = UsuarioSerializer(read_only=True)
+    criador = UsuarioSerializer(read_only=True)
+    tags = EtiquetaSerializer(many=True, read_only=True)
+    class Meta:
+        model = Tarefa
+        fields = ['id', 'titulo', 'descricao', 'coluna', 'responsavel', 'criador', 'prioridade', 'data_criacao', 'data_conclusao', 'tags']
+        read_only_fields = ['id']
+
+
+class ComentarioSerializer(serializers.ModelSerializer):
+    tarefa = TarefaSerializer(many=True, read_only=True)
+    autor = UsuarioSerializer(read_only=True)
+    class Meta:
+        model = Comentario
+        fields = ['id', 'tarefa', 'autor', 'texto', 'data_criacao']
+        read_only_fields = ['id']
+
 
 class ProjetoSerializer(serializers.ModelSerializer):
     proprietario = UsuarioSerializer(read_only=True)
@@ -15,3 +51,8 @@ class ProjetoSerializer(serializers.ModelSerializer):
         model = Projeto
         fields = ['id', 'nome', 'descricao', 'data_criacao', 'proprietario', 'membros']
         read_only_fields = ['id', 'data_criacao']
+
+
+
+
+
